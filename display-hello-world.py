6fae890e0af85e1e4564fbe0b3d7dcd8ff5e4b76
AndrewSwirski <andrew.swirski@hotmail.co.uk>
2024-05-22 20:05:33 +0100

    Update display-hello-world.py

--- display-hello-world.py
+++ display-hello-world.py
@@ -1,8 +1,7 @@
-import threading
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
 from luma.core.render import canvas
-import time
+from time import sleep
 
 
 serial = i2c(port=1, address=0x3C)
@@ -10,3 +9,4 @@ device = ssd1306(serial)
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
     draw.text((30, 40), "Hello World", fill="white")
+sleep(60)
